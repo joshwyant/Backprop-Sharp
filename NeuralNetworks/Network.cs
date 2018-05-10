0a3d17bb27e67a59d55281d2246635a14348b543
Josh Wyant <joshwyant91@gmail.com>
2018-05-09 23:22:13 -0700

    Using a numerics library for matrix math. Looks like it might be slightly faster.

--- NeuralNetworks/Network.cs
+++ NeuralNetworks/Network.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Single;
 
 namespace NeuralNetworks
 {
@@ -15,18 +18,19 @@ namespace NeuralNetworks
 
         public int[] Sizes { get; }
 
-        public LayerVector[] Biases { get; private set; }
+        public Vector<float>[] Biases { get; private set; }
 
-        public LayerMatrix[] Weights { get; private set; }
+        public Matrix<float>[] Weights { get; private set; }
         
         public Network(int[] sizes, Random r = null)
         {
             this.r = r ?? new Random();
+            var n = new Normal(this.r);
             Layers = sizes.Length;
             Sizes = sizes;
-            Biases = sizes.Skip(1).Select(y => this.r.CreateNormal(y)).ToArray();
+            Biases = sizes.Skip(1).Select(y => Vector.Build.Random(y, n)).ToArray();
             Weights = sizes.Take(Layers - 1).Zip(sizes.Skip(1), (x, y) => (layerFromSize: x, layerToSize: y))
-                .Select(pair => this.r.CreateNormal(pair.layerFromSize, pair.layerToSize)).ToArray();
+                .Select(pair => Matrix.Build.Random(pair.layerToSize, pair.layerFromSize, n)).ToArray();
             Activation = VectorUtilities.Sigmoid;
             ActivationPrime = VectorUtilities.SigmoidPrime;
         }
@@ -51,14 +55,14 @@ namespace NeuralNetworks
 
         private int Evaluate((float[] x, float[] y)[] testData)
         {
-            return testData.Select(pair => (x: FeedForward(new LayerVector(pair.x)).ArgMax().ToArray(), y: pair.y))
+            return testData.Select(pair => (x: FeedForward(Vector.Build.Dense(pair.x)).ArgMax().ToArray(), y: pair.y))
                 .Sum(pair => pair.x.SequenceEqual(pair.y) ? 1 : 0);
         }
 
         private void UpdateMiniBatch((float[] x, float[] y)[] miniBatch, float eta)
         {
-            var nablaB = Biases.Select(b => new LayerVector(b.Length)).ToArray();
-            var nablaW = Weights.Select(w => new LayerMatrix(w.Width, w.Height)).ToArray();
+            var nablaB = Biases.Select(b => Vector.Build.Dense(b.Count)).ToArray();
+            var nablaW = Weights.Select(w => Matrix.Build.Dense(w.RowCount, w.ColumnCount)).ToArray();
             foreach (var pair in miniBatch)
             {
                 var (deltaNablaB, deltaNablaW) = Backprop(pair.x, pair.y);
@@ -68,57 +72,59 @@ namespace NeuralNetworks
                     .Select(nwdnw => nwdnw.nw.Add(nwdnw.dnw)).ToArray();
             }
             Weights = Weights.Zip(nablaW, (x, y) => (w: x, nw: y))
-                .Select(pair => pair.w.Subtract(pair.nw.Scale(eta / miniBatch.Length))).ToArray();
+                .Select(pair => pair.w.Subtract(pair.nw.Multiply(eta / miniBatch.Length))).ToArray();
             Biases = Biases.Zip(nablaB, (x, y) => (b: x, nb: y))
                 .Select(pair => pair.b.Subtract(pair.nb.Multiply(eta / miniBatch.Length))).ToArray();
         }
 
-        private (LayerVector[], LayerMatrix[]) Backprop(float[] x, float[] y)
+        private (Vector<float>[], Matrix<float>[]) Backprop(float[] x, float[] y)
         {
-            var nablaB = Biases.Select(b => new LayerVector(b.Length)).ToArray();
-            var nablaW = Weights.Select(w => new LayerMatrix(w.Width, w.Height)).ToArray();
-            var activation = new LayerVector(x);
-            var activations = new List<LayerVector>(new[] { activation });
-            var zs = new List<LayerVector>();
+            var nablaB = Biases.Select(b => Vector.Build.Dense(b.Count)).ToArray();
+            var nablaW = Weights.Select(w => Matrix.Build.Dense(w.RowCount, w.ColumnCount)).ToArray();
+            var activation = Vector.Build.Dense(x);
+            var activations = new List<Vector<float>>(new[] { activation });
+            var zs = new List<Vector<float>>();
             foreach (var bw in Biases.Zip(Weights, (b, w) => (b: b, w: w)))
             {
-                var z = bw.b.Add(activation.Transform(bw.w));
+                var z = bw.b.Add(bw.w.Multiply(activation));
                 zs.Add(z);
-                activation = z.Apply(Activation);
+                activation = z.Map(Activation, Zeros.Include);
                 activations.Add(activation);
             }
             var delta = CostDerivative(activations[activations.Count - 1], y)
-                .Multiply(zs[zs.Count - 1].Apply(ActivationPrime));
+                .PointwiseMultiply(zs[zs.Count - 1].Map(ActivationPrime, Zeros.Include));
 
             nablaB[nablaB.Length - 1] = delta;
             // Below: multiply delta vector as a matrix with the transpose of the activation of the L-1 layer.
-            nablaW[nablaW.Length - 1] = delta.Multiply(activations[activations.Count - 2].Transpose());
+            nablaW[nablaW.Length - 1] = delta.ToColumnMatrix().Multiply(activations[activations.Count - 2]
+                .ToRowMatrix());
 
             for (var l = 2; l < Layers; l++)
             {
                 var z = zs[zs.Count - l];
-                var sp = z.Apply(ActivationPrime);
+                var sp = z.Map(ActivationPrime, Zeros.Include);
                 // Below: Multiply transpose of the weight matrix with the delta vector as a matrix, 
                 // which returns a vector (matrix Mx1), and store the hadamard (elementwise) product
                 // of that vector and the sigmoid prime vector back into the delta variable.
-                delta = Weights[Weights.Length - l + 1].Transpose().Multiply(delta).Multiply(sp);
+                delta = Weights[Weights.Length - l + 1].Transpose().Multiply(delta).PointwiseMultiply(sp);
                 nablaB[nablaB.Length - l] = delta;
-                nablaW[nablaW.Length - l] = delta.Multiply(activations[activations.Count - l - 1].Transpose());
+                nablaW[nablaW.Length - l] = delta.ToColumnMatrix().Multiply(activations[activations.Count - l - 1]
+                    .ToRowMatrix());
             }
 
             return (nablaB, nablaW);
         }
 
-        private LayerVector CostDerivative(LayerVector outputActivations, float[] y)
+        private Vector<float> CostDerivative(Vector<float> outputActivations, float[] y)
         {
-            return outputActivations.Subtract(new LayerVector(y));
+            return outputActivations.Subtract(Vector.Build.Dense(y));
         }
 
-        public LayerVector FeedForward(LayerVector a)
+        public Vector<float> FeedForward(Vector<float> a)
         {
             foreach (var (b, w) in Biases.Zip(Weights, (b, w) => (b, w)))
             {
-                a = a.Transform(w).Add(b).Apply(Activation);
+                a = w.Multiply(a).Add(b).Map(Activation, Zeros.Include);
             }
             return a;
         }
